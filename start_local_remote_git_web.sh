8698f9927472671e52bd8324d16eb14eecc5b307
Alex Autrey <aautrey@timcomp.com>
2023-07-22 23:25:58 -0500

    Removd Seperated Scripts. Created more user flexibility.

--- start_local_remote_git_web.sh
+++ start_local_remote_git_web.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-read -p "Site Name: " site
+echo "Assumes you have a ~/.ssh/config for your server"
+read -p "Server Name: " serverName
+read -p "Site Name no spaces" siteName
+read -p "Local Path to Site Files: " localPath
+cd ${localPath}
 git init
 git branch -m master
 git add .
@@ -7,9 +11,27 @@ sleep 1
 git commit -m "Initial Commit"
 sleep 1
 echo "Adding Remote"
+echo "Repo should not be accessible by the web server."
+read -p "Remote Repo Git Path: " repoPath
+read -p "Remote Apache/Nginx Path: " webPath
+
+if [ ssh ${serverName} '[ -d "'"${repoPath}"'" ] && echo "True" || echo "False"' -eq "False" ]: then
+    ssh ${serverName} "mkdir -p ${repoPath}"
+    ssh ${serverName} "cd ${repoPath} && git init"
+    ssh ${serverName} "cd ${repoPath} && git branch -m origin"
+    ssh ${serverName} "cd ${repoPath} && add ."
+    ssh ${serverName} 'cd '"${repoPath}"' && git commit -m "Initial Commit"'
+    ssh ${serverName} "touch ${repoPath}/hooks/post-receive"
+    ssh ${serverName} 'echo "git --work-tree='${webPath}' --git-dir='"${repoPath}"' checkout -f master" >> '"${repoPath}"'/hooks/post-receive'
+    ssh ${serverName} "chown -R www-data:www-data ${webPath}"
+fi
+
+if [ ssh ${serverName} '[ -d "'"${webPath}"'" ] && echo "True" || echo "False"' -eq "False" ]: then
+    ssh ${serverName} "mkdir -p ${webPath}"
+fi
+
 git remote add origin \
-read -p "Remote Repo Path: " repo
-ssh://WebServer/${repo}/$site.git
+ssh://${serverName}/${repo}/${siteName}.git
 git push --set-upstream origin master
 git push
 echo "First Commit Completed and Your all Set"
