f4bb9c1b3cdb4c73f7d0517a681966ecda74d3b0
Alex Autrey <shunobies@gmail.com>
2023-08-05 23:36:35 -0500

    Updated to resolve api issues

--- gdddclient.sh
+++ gdddclient.sh
@@ -2,43 +2,63 @@
 # Get godaddy api key https://developer.godaddy.com/keys
 # Make sure to chmod a+x this file before executing.
 
-FILE='~/gdddclient.conf'
+FILE="gdddclient.conf"
+logdate=$(date -u)
+logdest="gdddclient.log"
 
-if test -f "$FILE"; then
-  mapfile -t array <$FILE
-  mydomain=${array[0]}
-  myhostname=${array[1]}
-  gdapikey=${array[2]}
-  logdest="/var/log/gdddclient.log"
 
-  myip=`curl -s "https://api.ipify.org"`
-  #hostip= dig timcomp.com | grep -A 1 'ANSWER SECTION' | grep timcomp.com | awk '{print $5}'
+if [ -f ${FILE} ]; then
+	mapfile -t array <$FILE
+	domain=${array[0]}
+	name=${array[1]}
+	key=${array[2]}
+	secret=${array[3]}
+	port=1
+	type=A
+	ttl=600
+	weight=1
 
-  dnsdata=`curl -s -X GET -H "Authorization: sso-key ${gdapikey}" "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}"`
-  gdip=`echo $dnsdata | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
-  echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $myip, GoDaddy DNS IP is $gdip"
+	currentIp=$(curl -s "https://api.ipify.org")
 
-  if [ "$gdip" != "$myip" -a "$myip" != "" ]; then
-    echo "IP has changed!! Updating on GoDaddy"
-    curl -s -X PUT "https://api.godaddy.com/v1/domains/${mydomain}/records/A/${myhostname}" -H "Authorization: sso-key ${gdapikey}" -H "Content-Type: application/json" -d "[{\"data\": \"${myip}\"}]"
-    logger -p $logdest "Changed IP on ${hostname}.${mydomain} from ${gdip} to ${myip}"
-  fi
+	headers="Authorization: sso-key $key:$secret"
 
-  is_in_cron='~/ddclient.sh'
-  cron_entry=$(crontab -l 2>&1) || exit
-  new_cron_entry='*/10 * * * *    ~/gdddclient.sh > /dev/null'
+	result=$(curl -s -X GET -H "${headers}" \
+	"https://api.godaddy.com/v1/domains/${domain}/records/${type}/${name}")
 
-  if [[ "$cron_entry" != *"$is_in_cron"* ]]; then
-    printf '%s\n' "$cron_entry" "$new_cron_entry" | crontab -
-  fi
+	dnsIp=$(echo $result | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+	echo "dnsIp:" $dnsIp
+
+	# Get public ip address there are several websites that can do this.
+	echo "currentIp:" $currentIp
+
+	if [ "$dnsIp" != "$currentIp" ]; then
+		echo "${logdate}: Updating Record from Previous IP ${dnsIp} to Current IP ${currentIp}" >> $logdest
+		curl -X PUT "https://api.godaddy.com/v1/domains/${domain}/records/${type}/${name}" \
+                -H "accept: application/json" \
+                -H "Content-Type: application/json" \
+                -H "$headers" \
+				-d "[ { \"data\": \"${currentIp}\", \"port\": ${port}, \"priority\": 0, \"protocol\": \"string\", \"service\": \"string\", \"ttl\": ${ttl}, \"weight\": ${weight} } ]"
+	fi
+	if [ "$dnsIp" = "$currentIp" ]; then
+		echo "${logdate}: IP's are equal, no update required" >> $logdest
+	fi
+
+	is_in_cron='gdddclient.sh'
+	cron_entry=$(crontab -l 2>&1) || exit
+	new_cron_entry='*/10 * * * *    ~/gdddclient.sh > /dev/null'
+
+	if [[ "$cron_entry" != *"$is_in_cron"* ]]; then
+		printf '%s\n' "$cron_entry" "$new_cron_entry" | crontab -
+	fi
 else
-  touch $FILE
-  read -p 'Domain Name: ' mydomain
-  echo $mydomain >> $FILE
-  read -p 'Host Name: (Ex. Parked, www) ' myhostname
-  echo $myhostname >> $FILE
-  read -p 'API Key: ' apikey
-  read -p 'API Secret: ' apisecret
-  echo "${apikey}:${apisecret}" >> $FILE
-  echo "Log file located at /var/log/gdddclient.log"
+	touch ${FILE}
+	read -p 'Domain Name: ' mydomain
+	echo ${mydomain} >> ${FILE}
+	read -p 'Host Name: (Ex. Parked, www) ' myhostname
+	echo ${myhostname} >> ${FILE}
+	read -p 'API Key: ' apikey
+	read -p 'API Secret: ' apisecret
+	echo ${apikey} >> ${FILE}
+	echo ${apisecret} >> ${FILE}
+	echo "Log file located at /var/log/gdddclient.log"
 fi
