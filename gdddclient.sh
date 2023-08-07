6dd5e3ec9237298fbe36957e674e3981532df652
Alex Autrey <shunobies@gmail.com>
2023-08-06 22:06:21 -0500

    Still attempting to log

--- gdddclient.sh
+++ gdddclient.sh
@@ -23,9 +23,8 @@ fi
 
 function actions {
 	# Log Run
-	exec 3>&1 4>&2
-	trap 'exec 2>&4 1>&3' 0 1 2 3
-	exec 1>$logdest 2>&1
+	exec 1> ${logdest} 2>&1
+	set -x
 
 	mapfile -t array <$FILE
 	domain=${array[0]}
